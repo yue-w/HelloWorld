1868880d10b1f564cf14b6fe4512fcd03e46341e
ganlx <ganli2012@gmail.com>
2017-04-14 13:17:29 +0800

    hello world

---
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,16 @@
+// HelloWorld.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello world." << endl;
+
+    return 0;
+}
+
