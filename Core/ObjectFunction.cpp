cc40bd83e27003f269113ef4ff625a4a95204300
ganli2016 <ganli2012@gmail.com>
2017-05-20 16:40:31 +0800

    Add cpp files in Rcc include

--- Core/ObjectFunction.cpp
+++ Core/ObjectFunction.cpp
@@ -8,7 +8,7 @@ double ObjectFunction::Compute(const vector<double>& x) const
 {
 	auto x0 = x.front();
 	//Insert Function
-double fun=x0*2;
+double fun=x0*6;
 
 	cout << x0 << endl;
 	cout << fun << endl;
